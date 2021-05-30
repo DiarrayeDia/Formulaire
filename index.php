6a7c42b8e47396e3668c9b1dd4e3fe5ae258c496
DiarrayeDia <diarraye.dia@gmail.com>
2021-05-30 17:26:56 +0200

    set chekform function

--- index.php
+++ index.php
@@ -4,5 +4,24 @@
         if (!$_POST) {
             require 'form.php';
         } else {
+            $error = checkForm($_POST);
+        }
+
+        if ($error !== []) {
+            require 'form.php';
+        } else {
             require 'validation_form.php';
+        }
+
+        function checkForm(array $post)
+        {
+            $error = [];
+            if ($post['lastname'] === "" || strlen($post['lastname'] < 2)) {
+                $error['lastname'] = "Veuillez entrer un nom de famille valide (min 2 lettres) ";
+            }
+
+            if ($post['firstname'] === "" || strlen($post['firstname'] < 2)) {
+                $error['firstname'] = "Veuillez entrer un prénom valide (min 2 lettres) ";
+            }
+            return $error;
         }
