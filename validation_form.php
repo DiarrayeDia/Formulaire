1cdc22e144803dbdf3a5d5c765a667deb78bb00f
DiarrayeDia <diarraye.dia@gmail.com>
2021-06-05 17:57:51 +0200

    fixed function position

--- validation_form.php
+++ validation_form.php
@@ -1,17 +1,4 @@
 <?php
-function checkForm(array $post)
-{
-    $error = [];
-    if ($post['lastname'] === "" || strlen($post['lastname'] < 2)) {
-        $error['lastname'] = "Veuillez entrer un nom de famille valide (min 2 lettres) ";
-    }
-
-    if ($post['firstname'] === "" || strlen($post['firstname'] < 2)) {
-        $error['firstname'] = "Veuillez entrer un prénom valide (min 2 lettres) ";
-    }
-    return $error;
-}
-
 function rating(string $points)
 {
     $rate = '';
