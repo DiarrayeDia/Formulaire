2fcc67802c2264aecf5bb3849439637d4141857f
DiarrayeDia <diarraye.dia@gmail.com>
2021-06-05 17:39:54 +0200

    moved checkform function to validation file

--- index.php
+++ index.php
@@ -12,16 +12,3 @@
         } else {
             require 'validation_form.php';
         }
-
-        function checkForm(array $post)
-        {
-            $error = [];
-            if ($post['lastname'] === "" || strlen($post['lastname'] < 2)) {
-                $error['lastname'] = "Veuillez entrer un nom de famille valide (min 2 lettres) ";
-            }
-
-            if ($post['firstname'] === "" || strlen($post['firstname'] < 2)) {
-                $error['firstname'] = "Veuillez entrer un prénom valide (min 2 lettres) ";
-            }
-            return $error;
-        }
